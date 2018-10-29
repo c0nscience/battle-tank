efaea9f1ecaa9e36aa58535346ec331dc6ec8d80
Benjamin Herzig <ber.bherzig@gmail.com>
2018-10-29 23:43:28 +0100

    start moving the tank

--- BattleTank/Source/BattleTank/TankTrack.h
+++ BattleTank/Source/BattleTank/TankTrack.h
@@ -18,4 +18,6 @@ public:
 	UFUNCTION(BlueprintCallable, Category = "Input")
 	void SetThrottle(float Throttle);
 	
+	UPROPERTY(EditDefaultsOnly, Category = "Input")
+	float TrackMaxDrivingForce = 40000;
 };
